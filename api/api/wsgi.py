b5b346fc1868959617d9c465cbc116dc5d4f9401
nancy.james <nancynyokabi1995@gmail.com>
2020-04-14 22:06:24 +0300

    add requirements and update settings

--- api/api/wsgi.py
+++ api/api/wsgi.py
@@ -7,10 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-import os
+# import os
 
-from django.core.wsgi import get_wsgi_application
+# from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
+
+
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
