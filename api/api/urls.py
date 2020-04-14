b1cc8e6899df654528c25389fc15e3631ace7ef6
nancy.james <nancynyokabi1995@gmail.com>
2020-04-15 00:22:20 +0300

    update app.properties

--- api/api/urls.py
+++ api/api/urls.py
@@ -5,7 +5,7 @@ from estimator.views import Estimator, Logs
 urlpatterns = [
     path("api/v1/on-covid-19/logs", Logs.as_view()),
     path('api/v1/on-covid-19/<str:res_fmt>', Estimator.as_view()),
-    path('api/v1/on-covid-19', Estimator.as_view()),
+    path('api/v1/on-covid-19/', Estimator.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
